1072ed40e203d8ba5656418ab79293db806dd17a
William <william.marback@hotmail.com>
2020-09-03 11:12:46 +0200

    Spelare, bakgrund, tileset, kamera (pixel perfect)

---
+++ Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float speed = 2;
+    private float leftBound = -10;
+    private PlayerController playerControllerScript;
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,32 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
+    private Rigidbody2D playerRb2D;
+    public float gravityModifier;
+    public float jumpForce;
+    public bool isOnGround = true;
+    public bool gameOver = false;
     void Start()
     {
-        
+        playerRb2D = GetComponent<Rigidbody2D>();
+        Physics2D.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb2D.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
     }
 }
